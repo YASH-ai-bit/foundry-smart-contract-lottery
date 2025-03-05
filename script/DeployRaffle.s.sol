25f1982ff12b656abf2a30d3e89d83707a8df9f6
Yash-ai-bit <gujjaryash7089@gmail.com>
2025-03-05 20:08:03 +0000

    Mocks Deployed ✅

---
+++ script/DeployRaffle.s.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import {Script} from "forge-std/Script.sol";
+import {Raffle} from "src/Raffle.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+contract DeployRaffle is Script {
+    function run() public {}
+    
+    function deployContract() public returns(Raffle, HelperConfig) {}
+}
